3eb24cdcc03bf81372f0f945273c7328bdc30e8c
Tom-Kim <kyuokami9@gmail.com>
2014-12-04 15:47:36 -0500

    Added skeleton funcitons for each flag

--- stock_exercise/src/stock_exercise/core.clj
+++ stock_exercise/src/stock_exercise/core.clj
@@ -1,6 +1,46 @@
 (ns stock-exercise.core
   (:gen-class))
 
+"Flags"
+(defn daily-winners
+  "Prints stocks that had the highest % gain or lowest % loss for each day"
+  [stocks & args]
+  )
+
+(defn weekly-winners
+  "Prints stocks that had the highest % gain or lowest % loss for each week
+Fri-Fri"
+  [stocks & args]
+  )
+
+(defn tops
+  "Prints top 10 closing prices and dates for each stock"
+  [stocks & args]
+  )
+
+(defn bottoms
+  "Prints bottom 10 closing prices and dates for each stock"
+  [stocks & args]
+  )
+
+(defn price-graph
+  "Displays a line chart plotting daily closing prices for all stocks"
+  [stocks & args]
+  )
+
+(defn weekly-graph
+  "Displays candlestick chart plotting weekly price activity for all stocks"
+  [stocks & args]
+  )
+
+(defn money-game
+  "Simulates optimal investing for $100k based on closing day prices of stocks"
+  [stocks & args]
+  )
+
+"Given a set of stock symbols and flags as input, program should download the
+daily closing data for each of the stocks for the last year and implement
+flags given"
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
