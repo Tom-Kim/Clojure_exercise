a803b5cfdfb054f475409c16a1472ad27bc3116a
Tom-Kim <kyuokami9@gmail.com>
2014-12-05 15:56:54 -0500

    Added time dependency and ideas for functions

--- stock_exercise/src/stock_exercise/core.clj
+++ stock_exercise/src/stock_exercise/core.clj
@@ -4,26 +4,47 @@
 
 (use 'clj-quandl.core)
 
+"Quandl token to do API calls"
+(def auth-token "-xvzyytDom9qJHjjsZMv")
+
+(def quandl-api "http://www.quandl.com/api/v1/datasets/WIKI")
+
 "Flags"
 (defn daily-winners
   "Prints stocks that had the highest % gain or lowest % loss for each day"
   [stocks & args]
+  "get stock data for past year and then compare closing prices for each day for
+given stock symbols
+
+loop -> compare -> print"
+  
   )
 
 (defn weekly-winners
   "Prints stocks that had the highest % gain or lowest % loss for each week
 Fri-Fri"
   [stocks & args]
+  "get stock data for the past year and then average change in closing price for each week.  Compare deltas and print winners
+
+loop -> daily-winners -> average -> compare delta -> print"
   )
 
 (defn tops
   "Prints top 10 closing prices and dates for each stock"
   [stocks & args]
+  "get stick data for the past year and then compare all closing prices for
+highest ones.  Print with date
+
+loop -> compare closing prices -> print top 10"
   )
 
 (defn bottoms
   "Prints bottom 10 closing prices and dates for each stock"
   [stocks & args]
+  "get stock data for the past year and then compare all closing prices for
+lowest ones.  Print with date
+
+loop -> compare closing prices -> print bottom 10"
   )
 
 (defn price-graph
